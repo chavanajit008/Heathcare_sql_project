2a2afefb3ab1d84242d3acee69d35cf2707c06b1
Ajit Chavan <chavanajit47@gmail.com>
2025-11-01 16:51:27 +0530

    Add files via upload

---
+++ Healthcare_data_analysis.sql
@@ -0,0 +1,11 @@
+Healthcare data analysis with SQL
+
+1. total number of patients-
+select count(*) from Patients;
+
+2. average age of the patients-
+select avg(Age) from Patients;
+
+3. patients by condition-
+select Medical_Condition, count(*) from Patients
+group by 1;
